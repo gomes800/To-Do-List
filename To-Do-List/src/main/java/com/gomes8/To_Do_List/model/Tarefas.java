14e3c6bd6b58832431fbb16579ed95e137546ffa
barox <joaogomesleite800@gmail.com>
2024-11-27 12:50:24 -0300

    Primeiro commit

---
+++ To-Do-List/src/main/java/com/gomes8/To_Do_List/model/Tarefas.java
@@ -0,0 +1,62 @@
+package com.gomes8.To_Do_List.model;
+
+public class Tarefas {
+
+    private Long id;
+    private String nome;
+    private String descricao;
+    private boolean realizado;
+    private String prioridade;
+
+    public Tarefas() {}
+
+    public Tarefas(Long id, String nome, String descricao, boolean realizado, String prioridade) {
+        this.id = id;
+        this.nome = nome;
+        this.descricao = descricao;
+        this.realizado = realizado;
+        this.prioridade = prioridade;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public boolean isRealizado() {
+        return realizado;
+    }
+
+    public void setRealizado(boolean realizado) {
+        this.realizado = realizado;
+    }
+
+    public String getPrioridade() {
+        return prioridade;
+    }
+
+    public void setPrioridade(String prioridade) {
+        this.prioridade = prioridade;
+    }
+
+
+}
