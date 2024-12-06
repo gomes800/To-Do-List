5847dfb3414ce8dfff7793c074daf2f24b86847b
barox <joaogomesleite800@gmail.com>
2024-12-06 15:24:55 -0300

    Mais testes unitários

--- src/test/java/com/gomes8/To_Do_List/service/ServiceTest.java
+++ src/test/java/com/gomes8/To_Do_List/service/ServiceTest.java
@@ -1,9 +1,11 @@
 package com.gomes8.To_Do_List.service;
 
+import com.gomes8.To_Do_List.exceptions.ResourceNotFoundException;
 import com.gomes8.To_Do_List.model.Tarefa;
 import com.gomes8.To_Do_List.model.enums.Prioridade;
 import com.gomes8.To_Do_List.repositories.TarefasRepository;
 import com.gomes8.To_Do_List.services.TarefasService;
+import jakarta.persistence.EntityNotFoundException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
@@ -15,8 +17,8 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.when;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 public class ServiceTest {
 
@@ -48,7 +50,7 @@ public class ServiceTest {
 
     @Test
     public void deveAdicionarNovaTarefa() {
-        Tarefa novaTarefa = new Tarefa(null, "Tarefa 3", "Teste 3", false, Prioridade.BAIXA );
+        Tarefa novaTarefa = new Tarefa(null, "Tarefa 3", "Teste 3", false, Prioridade.BAIXA);
         Tarefa tarefaSalva = new Tarefa(3L, "Tarefa 3", "Teste 3", false, Prioridade.BAIXA);
 
         when(tarefasRepository.save(novaTarefa)).thenReturn(tarefaSalva);
@@ -57,6 +59,49 @@ public class ServiceTest {
 
         assertEquals("Tarefa 3", resultado.getNome(), "O título da tarefa adicionada está incorreto.");
         assertEquals(3L, resultado.getId(), "O ID da tarefa adicionada está incorreto.");
+    }
+
+    @Test
+    public void deveAtualizarUmaTarefa() {
+        Long idTarefa = 1L;
+
+        Tarefa tarefaExistente = new Tarefa(idTarefa, "Tarefa antiga", "Descrição antiga", false, Prioridade.ALTA);
+
+        Tarefa tarefaAtualizada = new Tarefa(5L, "Tarefa atualizada", "Descrição atualizada", false, Prioridade.BAIXA);
+
+        when(tarefasRepository.getReferenceById(idTarefa)).thenReturn(tarefaExistente);
+
+        when(tarefasRepository.save(tarefaExistente)).thenReturn(tarefaExistente);
+
+        Tarefa resultado = tarefasService.update(idTarefa, tarefaAtualizada);
+
+        assertEquals("Tarefa atualizada", resultado.getNome());
+        assertEquals("Descrição atualizada", resultado.getDescricao());
+        assertEquals(Prioridade.BAIXA, resultado.getPrioridade());
+        assertFalse(resultado.isRealizado());
+
+        verify(tarefasRepository, times(1)).getReferenceById(idTarefa);
+        verify(tarefasRepository, times(1)).save(tarefaExistente);
+    }
+
+    @Test
+    public void deveLancarExcecaoQuandoTarefaNaoForEncontrada() {
+        Long idInvalido = 999L;
+
+        when(tarefasRepository.getReferenceById(idInvalido)).thenThrow(ResourceNotFoundException.class);
+
+        assertThrows(ResourceNotFoundException.class, () -> tarefasService.update(idInvalido, new Tarefa()));
+    }
+
+    @Test
+    public void deletarTarefaPorId() {
+        Long idTarefa = 1L;
+
+        doNothing().when(tarefasRepository).deleteById(idTarefa);
+
+        tarefasService.delete(idTarefa);
+
+        verify(tarefasRepository, times(1)).deleteById(idTarefa);
     }
 
 
